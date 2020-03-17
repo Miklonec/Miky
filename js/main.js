d13db79030a13bbd285b3b7339119d8bcb27615c
web-intelligent <61443172+web-intelligent@users.noreply.github.com>
2020-03-17 21:40:30 +0600

    Изменение зума

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-var player = document.getElementById("miky");
-player.playbackRate = 0.5;
+// var player = document.getElementById("miky");
+// player.playbackRate = 0.5;
+
+
+
+var myZoom = document.getElementById('my-zoom');
+var width = screen.width;
+
+if (width <= 1366) {
+    myZoom.style.zoom = "75%";
+}
+
+console.log(myZoom);
+
+console.log(width);
