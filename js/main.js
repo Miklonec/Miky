7995d5caa24e9c854de8c864d2dd00ef666b6409
Miklonec <kimgim71@gmail.com>
2020-03-18 09:27:47 +0300

    18.03.2

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var myZoom = document.getElementById('my-zoom');
 var width = screen.width;
 
 if (width <= 1366) {
-    myZoom.style.zoom = "90%";
+    myZoom.style.zoom = "80%";
 }
 
 console.log(myZoom);
